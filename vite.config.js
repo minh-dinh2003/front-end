8036031661370771d2b780fd7f12848351f1b9b5
Minh <aviciimm2003@gmail.com>
2024-08-30 22:26:56 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://minh-dinh2003.github.io/front-end/",
+  base: "https://minh-dinh2003.github.io/src/main.jsx",
 })
